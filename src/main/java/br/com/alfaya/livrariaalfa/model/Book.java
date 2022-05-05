e9b8caf3d0b8aad7d0631c09dc513c2f885fd2e6
Lucas Larangeira <lukearch.sh@gmail.com>
2022-05-05 14:35:35 -0300

    book model finished

---
+++ src/main/java/br/com/alfaya/livrariaalfa/model/Book.java
@@ -0,0 +1,26 @@
+package br.com.alfaya.livrariaalfa.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Book {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private String author;
+  private String releaseDate;
+  private String language;
+  private String dimensions;
+  private String isbn10;
+  private String isbn13;
+  private Integer pagesLength;
+}
